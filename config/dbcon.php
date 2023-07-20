5357be05d398694ce25e5ef87ef41bf54f8745b5
Jelai2000 <70430470+Jelai2000@users.noreply.github.com>
2023-07-20 13:37:24 +0800

    Add files via upload

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,1 +1,28 @@
+<?php
+  
+  $host ="localhost";
+  $username = "root";
+  $password = "";
+  $database = "cupcake";
 
+  // $host ="localhost";
+  // $username = "root";
+  // $password = "";
+  // $database = "gtpcom_desain";
+
+  //  $base_site_name = "cm";
+  // $base_url = "localhost/".$base_site_name;
+  // $base_name_site = "Desain";
+
+  //CREATING DATABASE CONNECTION
+  $con = mysqli_connect($host, $username, $password, $database);
+  
+
+  //CHECK DATABASE CONNECTION
+  if(!$con)
+  {
+    die("Connection Failed: ". mysqli_connect_error());
+  }
+
+  
+?>
