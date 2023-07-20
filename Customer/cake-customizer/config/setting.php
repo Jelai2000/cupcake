46bb85faf64e7659d82223dbfc4f04e2ba49a094
Jelai2000 <70430470+Jelai2000@users.noreply.github.com>
2023-07-20 13:44:49 +0800

    Add files via upload

--- Customer/cake-customizer/config/setting.php
+++ Customer/cake-customizer/config/setting.php
@@ -1,1 +1,18 @@
 
+
+<?php
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+  
+  $host ="localhost";
+  $username = "root";
+  $password = "";
+  $database = "gtpcom_desain";
+
+  //CREATING DATABASE CONNECTION
+  $con = mysqli_connect($host, $username, $password, $database);
+
+  $base_site_name = "cm";
+  $base_url = "localhost/".$base_site_name;
+  $base_name_site = "Customization";
+  
+?>  
