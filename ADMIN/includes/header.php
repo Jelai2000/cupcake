e3fe480a07910ef9ba8bad20dbae1ec9c765b72f
Jelai2000 <70430470+Jelai2000@users.noreply.github.com>
2023-07-20 13:32:11 +0800

    Add files via upload

---
+++ ADMIN/includes/header.php
@@ -0,0 +1,54 @@
+ <!-- <?php session_start(); ?>  -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png"> -->
+ <link rel="stylesheet" href="assets/css/customm.css">
+
+  <!-- GOOGLE FONTS --><!--HINDI GUMAGANA -->
+  <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bassa+Vah&display=swap" rel="stylesheet"> -->
+
+
+
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- Nucleo Icons 
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />-->
+  
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+
+  <!-- CSS Files -->
+  <link id="pagestyle" href="assets/css/material-dashboard.min.css" rel="stylesheet" />
+   <link href="assets/css/customm.css" rel="stylesheet" type="text/css" /> <!--HINDI GUMAGANA -->
+
+  <!-- CSS OF ADMIN HEADER -->
+  <link rel="stylesheet" type="text/css" href="assets/css/admin-header.css" />
+  
+  <!-- ALERTIFYJS -->
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
+
+  <style>
+    .form-select{
+    border: 1px solid #b3a1a1 !important;
+    padding: 8px, 10px;
+}
+  </style>
+</head>
+
+<body class="">
+  
+  <?php include('sidebar.php');?>
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+  <?php include('navbar.php');?>
